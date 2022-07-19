0bb07e34969bb88b9414f8e754f9a93656ed586d
yanis112 <90922910+yanis112@users.noreply.github.com>
2022-07-19 11:46:15 +0200

    Update autoscale.py

--- autoscale.py
+++ autoscale.py
@@ -5,7 +5,7 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=2)
 def job():
-    print 'This job is run every minute.'
+    print('This job is run every minute.')
 
 
 sched.start()
