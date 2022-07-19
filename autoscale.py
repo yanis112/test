693a4b9f71dd3f9f8e6186118222d83a3c8aeb31
yanis112 <90922910+yanis112@users.noreply.github.com>
2022-07-19 11:57:54 +0200

    Update autoscale.py

--- autoscale.py
+++ autoscale.py
@@ -3,7 +3,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=2)
+@sched.scheduled_job('interval', minutes=5)
 def job():
     print('This job is run every minute.')
 
